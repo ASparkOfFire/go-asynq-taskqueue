0ea1f5501f208612b55737f4dfef43b0c14404fe
asparkoffire <kabir.47.chd@gmail.com>
2023-12-06 14:09:32 +0530

    add flag for listenAddr

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
+	"github.com/asparkoffire/go-asynq-taskqueue/config"
 	"github.com/asparkoffire/go-asynq-taskqueue/transport"
 	"github.com/asparkoffire/go-asynq-taskqueue/worker"
 	"log"
 )
 
 func main() {
+	listenAddr := flag.String("listenAddr", config.HTTPAddr, "Listen address for HTTP transport.")
+	flag.Parse()
+
 	go func() {
-		log.Fatal(transport.StartServer())
+		log.Fatal(transport.StartServer(listenAddr))
 	}()
 	worker.Worker()
 }
--- transport/http.go
+++ transport/http.go
@@ -1,18 +1,17 @@
 package transport
 
 import (
-	"github.com/asparkoffire/go-asynq-taskqueue/config"
 	"github.com/asparkoffire/go-asynq-taskqueue/tasks"
 	"net/http"
 )
 
-func StartServer() error {
+func StartServer(listenAddr *string) error {
 	http.HandleFunc("/task", tasks.HandleTask)
 	http.HandleFunc("/status", tasks.HandleGetTask)
 	http.HandleFunc("/monitor", tasks.HandleMonitorTaskWS)
 	http.HandleFunc("/download", tasks.HandleDownloadImage)
 
-	if err := http.ListenAndServe(config.HTTPAddr, nil); err != nil {
+	if err := http.ListenAndServe(*listenAddr, nil); err != nil {
 		return err
 	}
 	return nil
